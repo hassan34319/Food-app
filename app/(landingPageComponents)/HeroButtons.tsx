020a3052bbe9ba1c379b6c94bbce83643b19eb61
hassan34319 <mh08062@st.habib.edu.pk>
2024-02-22 15:31:23 +0500

    Fixed hero buttons

--- app/(landingPageComponents)/HeroButtons.tsx
+++ app/(landingPageComponents)/HeroButtons.tsx
@@ -11,7 +11,7 @@ function HeroButtons({}: Props) {
       <button className="bg-transparent text-white border-white border-[1px] rounded-3xl px-1 md:py-2 py-1 md:px-[0.4rem] lg:px-4 lg:py-2 lg:text-xxs md:text-xxs xl:text-xs text-xxss shadow-lg font-bold">
         ANRUFEN
       </button>
-      <button className="bg-transparent text-white bg-[#8bcda6] rounded-3xl px-1 py-1  md:py-2 md:px-[0.4rem] lg:px-4 lg:py-2 lg:text-xxs xl:text-xs md:text-xxs text-xxss drop-shadow-2xl custom-shadow font-bold">
+      <button className=" text-white bg-[#8bcda6] rounded-3xl px-1 py-1  md:py-2 md:px-[0.4rem] lg:px-4 lg:py-2 lg:text-xxs xl:text-xs md:text-xxs text-xxss drop-shadow-2xl custom-shadow font-bold">
         FOODTRUCK BUCHEN
       </button>
     </div>
